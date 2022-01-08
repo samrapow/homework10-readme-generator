c8b3308087207b488f9383686bbe4a998967cc6d
Sam Rapowitz <samrapow@gmail.com>
2022-01-07 20:39:30 -0500

    Got all user input prompts to function

--- Develop/index.js
+++ Develop/index.js
@@ -13,6 +13,47 @@ function getUserInput() {
                 type: "input",
                 message: "Please type your Github username",
                 name: "username"
+            },
+            {
+                type: "input",
+                message: "Please type your email address",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "What title does your project have?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "What is the description of your project?",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "What packages does your project require?",
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: "What is your project used for?",
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: "Please list all contributers to your project",
+                name: "contributing"
+            },
+            {
+                type: "input",
+                message: "Which command does one use to run a test of your project?",
+                name: "tests"
+            },
+            {
+                type: "list",
+                message: "Which license does your project have?",
+                choices: ["MIT", "Mozilla", "Intel", "none"],
+                name: "license"
             }
         ])
 }
