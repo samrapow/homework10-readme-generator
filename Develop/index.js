52d01a782f4563285d60f075d31e8af598a0ff2f
Sam Rapowitz <samrapow@gmail.com>
2022-01-07 20:07:12 -0500

    Got first user input on inquirer working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+function getUserInput() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please type your Github username",
+                name: "username"
+            }
+        ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const userInput = getUserInput();
+    generateMarkdown(userInput);
+}
 
 // Function call to initialize app
 init();
